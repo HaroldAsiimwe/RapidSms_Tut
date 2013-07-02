9aa324834a8352ed9221632b94a4a8053e08cd89
Harold Asiimwe <harryasiimwe@gmail.com>
2013-07-02 15:45:42 +0300

    First Commit

---
+++ tut/myhandlers.py
@@ -0,0 +1,28 @@
+from rapidsms.contrib.handlers import KeywordHandler
+
+help_text = {
+	'aaa': 'Help for aaa is...',
+	'bbb': 'Help for bbb is ...',
+	'ccc': 'Help for ccc is ...',
+	}
+
+class HelpHandler(KeywordHandler):
+	"""docstring for HelpHandler"""
+	def help(self):
+		"""Invoked if someone just sends `HELP`.  We also call this
+        from `handle` if we don't recognize the arguments to HELP.
+        """
+        self.respond("Allowed commands are AAA, BBB, and CCC." 
+        	"Send HELP <command> for more help on a specific command.")
+
+   	def handle(self, text):
+   		"""Invoked if someone sends `HELP <any text>`"""
+   		text = text.strip().lower()
+   		if text == 'aaa':
+   			self.respond(help_text['aaa'])
+    	elif text == 'bbb':
+    		self.respond(help_text['bbb'])
+    	elif text == 'ccc':
+    		self.respond(help_text['ccc'])
+    	else:
+    		self.help()
